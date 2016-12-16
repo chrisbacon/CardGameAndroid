aa9fc83af9752e239869f3b4d23331076e0c2d8d
chrisbacon <bacon.chris@gmail.com>
2016-12-16 14:47:59 +0000

    created main android activity

--- app/src/main/java/com/codeclan/example/cardgameandroid/activities/mainActivity.java
+++ app/src/main/java/com/codeclan/example/cardgameandroid/activities/mainActivity.java
@@ -1,12 +1,26 @@
 package com.codeclan.example.cardgameandroid.activities;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
+import com.codeclan.example.cardgameandroid.R;
 import com.codeclan.example.cardgameandroid.cardGame.*;
 
 /**
  * Created by user on 16/12/2016.
  */
 public class mainActivity extends AppCompatActivity {
-    
+    TextView welcomeText;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        welcomeText = (TextView)findViewById(R.id.welcome_text);
+
+
+    }
 }
